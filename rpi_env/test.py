65dd21cca5f9832a3dbcbc608b11d5e81cff8583
niranjan <niranjan>
2024-12-21 11:21:18 +0100

    adding rpi campture

---
+++ rpi_env/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Capture a JPEG while still running in the preview mode.
+
+import time
+
+from picamera2 import Picamera2, Preview
+
+
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+
+preview_config = picam2.create_preview_configuration()
+capture_config = picam2.create_still_configuration()
+print (preview_config)
+print (capture_config)
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(10)
+
+picam2.switch_mode_and_capture_file(capture_config, "test_full.jpg")
